a86efb571f7100fd8c506df024253d0d325fcf0a
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-03 20:40:45 +0900

    チュートリアルバグ取り一歩
    二人目で接続した人が分身する

---
+++ Assets/Room for 1/GameManager1.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager1 : Photon.PunBehaviour
+    {
+        #region Public
+        static public GameManager1 Instance;
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+        #endregion
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.Log("PhotonNetwork : Loading Level : " + PhotonNetwork.room.PlayerCount);//playerCount => PlayerCount
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.room.PlayerCount);
+        }
+
+        #endregion
+
+        #region Photon Messages
+
+        ///// <summary>
+        ///// Called when the local player left the room. We need to load the launcher scene.
+        ///// </summary>
+        public void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        #region Photon Messages
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerConnected() " + other.NickName); // not seen if you're the player connecting //name => NickName
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects // name => NickName
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+                LoadArena();
+            }
+        }
+
+        #endregion
+        #region CallBack
+        void OnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+        #endregion
+        private void Start()
+        {
+            Instance = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                if (PlayerManager.LocalPlayerInstance == null)
+                {
+                    Debug.Log("We are Instantiating LocalPlayer from " + Application.loadedLevelName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.Log("Ignoring scene load for " + Application.loadedLevelName);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Room for 1/PlayerUI.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+namespace Com.MyCompany.MyGame
+{
+    public class PlayerUI : MonoBehaviour
+    {
+
+        #region Public Properties
+
+        [Tooltip("UI Text to display Player's Name")]
+        public Text PlayerNameText;
+
+        [Tooltip("UI Slider to display Player's Health")]
+        public Slider PlayerHealthSlider;
+
+        [Tooltip("Pixel offset from the player target")]
+        public Vector3 ScreenOffset = new Vector3(0f, 30f, 0f);
+        #endregion
+
+        #region Private Properties
+        PlayerManager _target;
+        float _characterControllerHeight = 0f;
+        Transform _targetTransform;
+        #endregion
+
+        #region MonoBehaviour Messages
+        private void Start()
+        {
+
+        }
+        void Update()
+        {
+            // Reflect the Player Health
+            if (PlayerHealthSlider != null)
+            {
+                PlayerHealthSlider.value = _target.Health;
+            }
+            // Destroy itself if the target is null, It's a fail safe when Photon is destroying Instances of a Player over the network
+            if (_target == null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+        void Awake()
+        {
+            this.GetComponent<Transform>().SetParent(GameObject.Find("Canvas").GetComponent<Transform>());
+        }
+        #endregion
+
+        #region Public Methods
+        public void SetTarget(PlayerManager target)
+        {
+            if (target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+            // Cache references for efficiency
+            _target = target;
+            if (PlayerNameText != null)
+            {
+                PlayerNameText.text = _target.photonView.owner.NickName;//name=>NickName
+            }
+            CharacterController _characterController = _target.GetComponent<CharacterController>();
+            // Get data from the Player that won't change during the lifetime of this Component
+            if (_characterController != null)
+            {
+                _characterControllerHeight = _characterController.height;
+            }
+        }
+        void LateUpdate()
+        {
+            // #Critical
+            // Follow the Target GameObject on screen.
+            if (_targetTransform != null)
+            {
+                Vector3 _targetPosition = _targetTransform.position;//Vector3追加
+                _targetPosition.y += _characterControllerHeight;
+                this.transform.position = Camera.main.WorldToScreenPoint(_targetPosition) + ScreenOffset;
+            }
+        }
+        #endregion
+
+    }
+}
